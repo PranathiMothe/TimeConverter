19dd29c9495bc5b0c4e642966c78319cf20a5a23
Mothe,Pranathi <s533572@ch140036m.nwmissouri.edu>
2019-02-05 12:50:51 -0600

    Initial Commit

---
+++ TimeConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeConverter
+//
+//  Created by Mothe,Pranathi on 2/5/19.
+//  Copyright © 2019 Mothe,Pranathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
